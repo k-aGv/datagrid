d66f0b47f71d2d7fdae0863e9dc275ed369a3f87
Dimitris Katikaridis <dkatikaridis@gmail.com>
2018-01-16 22:12:27 +0200

    Mehminator!

--- datagrid/datagrid/Form1.cs
+++ datagrid/datagrid/Form1.cs
@@ -18,7 +18,7 @@ namespace datagrid
             InitializeComponent();
         }
         string _database = Directory.GetCurrentDirectory() + "/db.txt";
-        int remaningRows = 0;
+        int remainingRows = 0;
         private void InitUI()
         {
             string[] days = new string[]
@@ -127,10 +127,10 @@ namespace datagrid
         private void btn_addRow_Click(object sender, EventArgs e)
         {
            
-            dataGridView1.Rows[dataGridView1.Rows.Count - 1 - remaningRows].DefaultCellStyle.BackColor = Color.White;
+            dataGridView1.Rows[dataGridView1.Rows.Count - 1 - remainingRows].DefaultCellStyle.BackColor = Color.LightGray;
             dataGridView1.Rows.Insert(0, "");
             dataGridView1.Rows[0].DefaultCellStyle.BackColor = Color.LightGreen;
-            remaningRows++;
+            remainingRows++;
         }
 
        
